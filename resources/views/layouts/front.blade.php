32a850e541fb9ec14a4cdd9c1d7a079de476fb0e
Johanes Surya <johanes.surya43@gmail.com>
2016-01-14 10:23:42 +0700

    Chnage template(more modern)

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -1,78 +1,194 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!--
+Author: W3layouts
+Author URL: http://w3layouts
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
 <head>
-<title>*FLORAL*</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<link href="{{asset('css/style.css')}}" rel="stylesheet" type="text/css" />
+<title>The Swiss Website Template | Home :: w3layouts</title>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href='//fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<!-- start slider -->
+	<link href="css/camera.css" rel="stylesheet" type="text/css" media="all" />
+	<script src="js/jquery.min.js"></script>
+    <script type='text/javascript' src="js/jquery.mobile.customized.min.js"></script>
+    <script type='text/javascript' src="js/jquery.easing.1.3.js"></script>
+    <script type='text/javascript' src="js/camera.min.js"></script>
+    <script>
+		jQuery(function(){
+
+			jQuery('#camera_wrap_2').camera({
+
+				loader: 'bar',
+				pagination: false,
+				thumbnails: true
+			});
+		});
+	</script>
+<!-- end slider -->
 </head>
 <body>
-<div id="container">
-  <div id="banner">
-    <h1>Company Name</h1>
-  </div>
-  <div id="navcontainer">
-    <ul id="navlist">
-      <li><a href="#">HOMEPAGE</a></li>
-      <li id="active"><a id="current" href="#">ABOUT</a></li>
-      <li><a href="#">GALLERY</a></li>
-      <li><a href="#">LINKS</a></li>
-      <li><a href="#">SERVICE</a></li>
-      <li><a href="#">CONTACT</a></li>
-    </ul>
-  </div>
-  <div id="sidebar">
-    <h2>Resources</h2>
-    <div class="navlist">
-      <ul>
-        <li><a href="#">Snapp Happy</a></li>
-        <li><a href="#">TODC</a></li>
-        <li><a href="#">OWD</a></li>
-        <li><a href="#">OSWD</a></li>
-        <li><a href="#">CSS play</a></li>
-        <li><a href="#">Andreas Viklund</a></li>
-        <li><a href="#">James Koster</a></li>
-      </ul>
-    </div>
-    <form action="#" method="post">
-      <fieldset>
-      <legend>Search</legend>
-      <div> <span>
-        <label for="txtsearch"> Find: <img src="{{asset('img/search.gif')}}" alt="search" /></label>
-        </span> <span>
-        <input type="text" value="demo only" name="txtsearch" title="Text input: search" id="txtsearch" size="20" />
-        </span> </div>
-      </fieldset>
-    </form>
-    <h2>Sub Menu</h2>
-    <div class="navlist">
-      <ul>
-        <li><a href="#">Link One</a></li>
-        <li><a href="#">Link Two</a></li>
-        <li><a href="#">Link Three</a></li>
-        <li><a href="#">Link Four</a></li>
-      </ul>
-    </div>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-  </div>
-  <div id="content">
-    <h2>Welcome to <span style="color:#B29B35;"> Floral</span> Template</h2>
-    <blockquote>This template has been tested in Mozilla and IE7. The page validates as XHTML 1.0 Transitional using valid CSS.<br />
-      For more FREE templates visit <a href="http://www.mitchinson.net">my website</a>.</blockquote>
-    <p>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-    <h2>Article One</h2>
-    <p class="info"><img class="noborder" src="img/flower.jpg" alt="flower" title="flower"/> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis odio. Suspendisse aliquet. Nunc porta rutrum urna. Morbi porttitor magna id velit. Morbi egestas. Etiam lorem sapien, vehicula in, dictum sed, accumsan tristique, justo. Nunc fringilla congue est. Donec mollis leo et arcu. In condimentum lacus non turpis. Morbi faucibus. Duis dapibus. Aenean lobortis. Ut nec metus non diam scelerisque euismod. Nulla facilisi. In laoreet, augue eu auctor scelerisque, urna orci pretium sem, nec pulvinar orci enim vel dui. Quisque sit amet arcu. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras vestibulum est in risus. Nulla facilisi.</p>
-    <p class="post"> <a href="#" class="readmore">Read more</a> <a href="#" class="comments">Comments (3)</a> <span class="date">April 26, 2007</span> </p>
-    <h2>Article Two</h2>
-    <p class="info"><img class="noborder" src="img/flower.jpg" alt="flower" title="flower"/> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Duis odio. Suspendisse aliquet. Nunc porta rutrum urna. Morbi porttitor magna id velit. Morbi egestas. Etiam lorem sapien, vehicula in, dictum sed, accumsan tristique, justo. Nunc fringilla congue est. Donec mollis leo et arcu. In condimentum lacus non turpis. Morbi faucibus. Duis dapibus. Aenean lobortis. Ut nec metus non diam scelerisque euismod. Nulla facilisi. In laoreet, augue eu auctor scelerisque, urna orci pretium sem, nec pulvinar orci enim vel dui. Quisque sit amet arcu. In hac habitasse platea dictumst. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Cras vestibulum est in risus. Nulla facilisi.</p>
-    <p class="post"> <a href="#" class="readmore">Read more</a> <a href="#" class="comments">Comments (3)</a> <span class="date">April 26, 2007</span> </p>
-  </div>
-  <div id="container-foot">
-    <div id="footer">
-      <p><a href="#">homepage</a> | <a href="mailto:denise@mitchinson.net">contact</a> | &copy; 2007 Anyone | Design by <a href="http://www.mitchinson.net"> www.mitchinson.net</a> | Licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/3.0/">Creative Commons Attribution 3.0 License</a></p>
-    </div>
-  </div>
+<!-- start header -->
+<div class="header-bg">
+<div class="wrap">
+<div class="header">
+		<div class="nav">
+				<ul>
+						<li class="a"><a class="active" href="index.html">Home</a> </li>
+						<li><a href="ask.html">Ask</a></li>
+						<li><a href="archive.html">Archive</a>  </li>
+						<li><a href="contact.html">Contact</a> </li>
+
+				</ul>
+		 </div>
+		 <div class="search">
+				<input type="text" name="s" class="textbox" value="Search" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Search';}">
+				<input type="submit" value="Subscribe" id="submit" name="submit">
+				<div id="response"> </div>
+		 </div>
+		<div class="clear"> </div>
 </div>
+</div>
+</div>
+<!-- end_header -->
+<div class="wrap">
+	<div class="logo">
+      <a href="index.html"><img src="images/logo.png" alt=""></a>
+    </div>
+	<div class="grid">
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text</p>
+	</div>
+	  <div class="clear"> </div>
+	  </div>
+	 <!-- start slider -->
+	<div class="slider">
+		<div class="fluid_container">
+        <div class="camera_wrap camera_magenta_skin" id="camera_wrap_2">
+            <div data-src="images/slider1.jpg">
+                <div class="camera_caption fadeFromBottom">
+                	<p><a href="index.html">VISTA</a></p>
+                    <h2>palce your description text here.great spot to tell about deals Lorem Ipsum has been the industry's standard dummy text.</h2>
+                </div>
+            </div>
+            <div data-src="images/slider2.jpg">
+                <div class="camera_caption fadeFromBottom">
+                	<p><a href="index.html">GREAT</a></p>
+                   <h2> It uses a light version of jQuery mobile, navigate the slides by swiping with your fingers Lorem Ipsum has been the industry's standard dummy text</h2>
+                </div>
+            </div>
+            <div data-src="images/slider4.jpg">
+                <div class="camera_caption fadeFromBottom">
+                	<p><a href="index.html">LIGHT</a></p>
+                    <h2> It uses a light version of jQuery mobile, navigate the slides by swiping with your fingers Lorem Ipsum has been the industry's standard dummy text</h2>
+                </div>
+            </div>
+        </div><!-- #camera_wrap_2 -->
+    </div><!-- fluid_container -->
+    <div class="clear"></div>
+	</div>
+ 	   <!--start-grids-->
+	 	  <div class="top-bg">
+	 	  <div class="wrap">
+		 	  <div class="top-cont">
+		          <h2>Lorem Ipsum is simply dummy text of the printing and typesetting industry</h2>
+		         <div class="f_nav1">
+						<ul>
+							<li><p><i class="icon1"> </i><span>4 Jan 24</span><p></li>
+							<li ><a href="#"> <i class="icon2"> </i> <span>Text</span></a></li>
+					  		<li ><a href="#"> <i class="icon3"> </i><span>3 Comments</span></a></li>
+						</ul>
+						<div class="clear"></div>
+				   </div>
+		          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged</p>
+			      <h6><a href="#">READ MORE</a></h6>
+			  </div>
+	   </div>
+	  </div>
+	   <div class="middle-bg">
+	   <div class="wrap">
+		   <div class="middle-cont">
+
+			   	 <div class="f_nav2">
+						<ul>
+							<li><p> <i class="icon1"> </i><span>4 Jan 24</span></p></li>
+							<li ><a href="#"> <i class="icon2"> </i> <span>Quote</span></a></li>
+					  		<li ><a href="#"> <i class="icon3"> </i><span>3 Comments</span></a></li>
+						</ul>
+						<div class="clear"></div>
+				   </div>
+		          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+			      <h6><a href="#">Haruki Murakami</a></h6>
+		   </div>
+	   </div>
+	   </div>
+	   <div class="bottom-bg">
+	   <div class="wrap">
+		   <div class="bottom-cont">
+			   	  <div class="f_nav3">
+						<ul>
+							<li><p> <i class="icon1"> </i><span>4 Jan 24</span></p></li>
+							<li ><a href="#"> <i class="icon2"> </i> <span>Video</span></a></li>
+					  		<li ><a href="#"> <i class="icon3"> </i><span>3 Comments</span></a></li>
+						</ul>
+						<div class="clear"></div>
+				   </div>
+		         <iframe src="//player.vimeo.com/video/83907617?title=0&amp;byline=0&amp;portrait=0&amp;badge=0" width="950" height="475" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen>
+		         </iframe>
+		   </div>
+	  </div>
+	  </div>
+	   <div class="footer-top-bg">
+	   <div class="wrap">
+	   	    <div class="footer-top">
+			   	  <div class="f_nav4">
+						<ul>
+							<li><p> <i class="icon1"> </i><span>4 Jan 24</span></p></li>
+							<li ><a href="#"> <i class="icon2"> </i> <span>Image</span></a></li>
+					  		<li ><a href="#"> <i class="icon3"> </i><span>3 Comments</span></a></li>
+						</ul>
+						<div class="clear"></div>
+				   </div>
+		         <img src="images/img.jpg"  alt=""/>
+		    </div>
+	   </div>
+	   </div>
+	   <div class="footer-middle-bg">
+	   <div class="wrap">
+	   	  <div class="footer-middle">
+		   	  <div class="f_nav5">
+						<ul>
+							<li><p> <i class="icon1"> </i><span>4 Jan 24</span></p></li>
+							<li ><a href="#"> <i class="icon2"> </i> <span>Music</span></a></li>
+					  		<li ><a href="#"> <i class="icon3"> </i><span>3 Comments</span></a></li>
+						</ul>
+						<div class="clear"></div>
+				   </div>
+	        <iframe src="//player.vimeo.com/video/24894937" width="950" height="180" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+		  </div>
+	  </div>
+	  </div>
+	  <!-- footer-bottom -->
+	<div class="footer-bottom">
+		 <div class="wrap">
+				 <div class="copy-right">
+					<p>&copy; 2014 Swiss. All rights reserved | Template by <a href="http://w3layouts.com/">  w3layouts</a></p>
+				</div>
+			 	<div class="social-icons">
+			   		  	<ul>
+				 			  <li class="icon1"><a href="#" target="_blank"> </a></li>
+						      <li class="icon2"><a href="#" target="_blank"> </a></li>
+						      <li class="icon3"><a href="#" target="_blank"> </a></li>
+						      <li class="icon4"><a href="#" target="_blank"> </a></li>
+						      <li class="icon5"><a href="#" target="_blank"> </a></li>
+						      <div class="clear"></div>
+				     	</ul>
+				</div>
+	     </div>
+       <div class="clear"> </div>
+    </div>
 </body>
 </html>
